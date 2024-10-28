c9289e8eed4a86112d039087ec40a684ed110065
Emerson Amorim <emerson_tecno@hotmail.com>
2024-10-28 09:26:02 -0300

    Cadastro Usuários Microservices - Node.js, MySQL, Redis, JWT, Prometheus e Grafana 🚀 🔄 🌐

--- src/infrastructure/database/MySQLConnection.js
+++ src/infrastructure/database/MySQLConnection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST || 'mysql-db', // Nome do serviço Docker
   user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'Elas1981XP#',
+  password: process.env.MYSQL_PASSWORD || 'sua-senha',
   database: process.env.MYSQL_DATABASE || 'usuariodb',
   waitForConnections: true,
   connectionLimit: 10,
