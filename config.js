f8139ce10bc457680f8dc67d91f4609d4c659b3f
Emerson Amorim <emerson_tecno@hotmail.com>
2024-10-28 09:25:42 -0300

    Cadastro Usuários Microservices - Node.js, MySQL, Redis, JWT, Prometheus e Grafana 🚀 🔄 🌐

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || 'usuariodb',
-    password: process.env.DB_PASSWORD || 'Elas1981XP#',
+    password: process.env.DB_PASSWORD || 'sua-senha',
     database: process.env.DB_NAME || 'usuariodb',
   },
   redis: {
